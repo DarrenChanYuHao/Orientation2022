98a0bccc991163dc17e59f14da871c8b00cf8b55
LimJH272 <94665958+LimJH272@users.noreply.github.com>
2022-05-27 21:34:46 +0800

    Update popup.js
    Added Javascript for hamburger menu popup

--- popup.js
+++ popup.js
@@ -1,19 +1,31 @@
 // Get the modal
+var sideMenu = document.getElementById("side-Menu");
+
 var rubPopUp = document.getElementById("rubPopUp");
 
 // Get the button that opens the modal
+var hamburgMenu = document.getElementById("hamburg-menu");
+
 var rubBtn = document.getElementById("rubBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("closed")[0];
 
 // When the user clicks on the button, open the modal
+hamburgMenu.onclick = function() {
+  sideMenu.style.display = "block";
+}
+
 rubBtn.onclick = function() {
     rubPopUp.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+menuClose.onclick = function() {
+  sideMenu.style.display = "none";
+}
+
+rubClose.onclick = function() {
     rubPopUp.style.display = "none";
 }
 
@@ -21,5 +33,8 @@ span.onclick = function() {
 window.onclick = function(event) {
   if (event.target == rubPopUp) {
     rubPopUp.style.display = "none";
+  } else if (event.target == sideMenu) {
+    sideMenu.style.display = "none";
   }
+}
 }
