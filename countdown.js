0d03d70a83152b85fb0d15c61f8116aa8336c8af
sihui3466 <103938218+sihui3466@users.noreply.github.com>
2022-05-29 20:48:18 +0800

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 const orientation_date='08 Sept 2022 00:00:00';
 
-function countdown(){
+const countdown= setInterval(function(){
     const newdate=new Date(orientation_date).getTime()
     const currentdate=new Date().getTime()
     const datediff= (newdate-currentdate)/1000 //difference between the orientation start date and current time in milliseconds
@@ -11,14 +11,15 @@ function countdown(){
     const minutes=Math.trunc((datediff/60)%60)
     const seconds=Math.trunc(datediff % 60)
     console.log(days)
-
+    if (seconds < 0) {
+        clearInterval(countdown);
+        return;
+      }
     document.querySelector('.days').innerHTML= days
     document.querySelector('.hours').innerHTML= hours
     document.querySelector('.minutes').innerHTML= minutes
     document.querySelector('.seconds').innerHTML= seconds
-}
-
-setInterval(countdown,1000) //run for every second (1000 millisecond)
+},1000) //run for every second (1000 millisecond)
 
 //scroll-up function
 var scrolltop = document.querySelector(".scrollup");
