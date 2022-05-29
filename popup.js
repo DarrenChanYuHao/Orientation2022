e1befd53d55af0134bced0e697f57bd7c17da1c4
sihui3466 <103938218+sihui3466@users.noreply.github.com>
2022-05-29 20:18:51 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -22,6 +22,18 @@ rubBtn.onclick = function() {
     rubPopUp.style.display = "block";
 }
 
+jadBtn.onclick = function() {
+  jadPopUp.style.display = "block";
+}
+
+citBtn.onclick = function() {
+  citPopUp.style.display = "block";
+}
+
+aquaBtn.onclick = function() {
+  aquaPopUp.style.display = "block";
+}
+
 // When the user clicks on <span> (x), close the modal
 menuClose.onclick = function() {
   sideMenu.style.display = "none";
@@ -31,10 +43,44 @@ rubClose.onclick = function() {
     rubPopUp.style.display = "none";
 }
 
+jadClose.onclick = function() {
+  jadPopUp.style.display = "none";
+}
+citClose.onclick = function() {
+  citPopUp.style.display = "none";
+}
+aquaClose.onclick = function() {
+  aquaPopUp.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == rubPopUp) {
     rubPopUp.style.display = "none";
+  } else if (event.target == sideMenu) {
+    sideMenu.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == jadPopUp) {
+    jadPopUp.style.display = "none";
+  } else if (event.target == sideMenu) {
+    sideMenu.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == citPopUp) {
+    citPopUp.style.display = "none";
+  } else if (event.target == sideMenu) {
+    sideMenu.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+  if (event.target == aquaPopUp) {
+    aquaPopUp.style.display = "none";
   } else if (event.target == sideMenu) {
     sideMenu.style.display = "none";
   }
