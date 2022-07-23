3c57205b4ee867f20c4dee1771a407891d59a00c
LimJH272 <limjiehan02@gmail.com>
2022-07-23 20:24:14 +0800

    Multiple edits
    1. Zoom in/out animation
    2. Improve visibility of popup on large mobile devices / in landscape mode

--- popup.js
+++ popup.js
@@ -40,21 +40,25 @@ hamburgMenu.onclick = function() {
 rubBtn.onclick = function() {
   rubPopUp.style.display = "block";
   setTimeout(() => {rubPopUp.style.opacity = '1';}, 1);
+  setTimeout(() => {rubWindow.classList.add('show');}, 1);
 }
 
 jadBtn.onclick = function() {
   jadPopUp.style.display = "block";
-  setTimeout(() => {jadPopUp.style.opacity = '1';}, 1);  
+  setTimeout(() => {jadPopUp.style.opacity = '1';}, 1); 
+  setTimeout(() => {jadWindow.classList.add('show');}, 1); 
 }
 
 citBtn.onclick = function() {
   citPopUp.style.display = "block";
   setTimeout(() => {citPopUp.style.opacity = '1';}, 1);
+  setTimeout(() => {citWindow.classList.add('show');}, 1);
 }
 
 aquaBtn.onclick = function() {
   aquaPopUp.style.display = "block";
   setTimeout(() => {aquaPopUp.style.opacity = '1';}, 1);
+  setTimeout(() => {aquaWindow.classList.add('show');}, 1);
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -65,19 +69,23 @@ menuClose.onclick = function() {
 }
 
 rubClose.onclick = function() {
+  rubWindow.classList.remove('show');
   setTimeout(() => {rubPopUp.style.opacity = '0';}, 1);
   setTimeout(() => {rubPopUp.style.display = "none";}, 300);
 }
 
 jadClose.onclick = function() {
+  jadWindow.classList.remove('show');
   setTimeout(() => {jadPopUp.style.opacity = '0';}, 1);
   setTimeout(() => {jadPopUp.style.display = "none";}, 300);
 }
 citClose.onclick = function() {
+  citWindow.classList.remove('show');
   setTimeout(() => {citPopUp.style.opacity = '0';}, 1);
   setTimeout(() => {citPopUp.style.display = "none";}, 300);
 }
 aquaClose.onclick = function() {
+  aquaWindow.classList.remove('show');
   setTimeout(() => {aquaPopUp.style.opacity = '0';}, 1);
   setTimeout(() => {aquaPopUp.style.display = "none";}, 300);
 }
@@ -85,15 +93,19 @@ aquaClose.onclick = function() {
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == rubPopUp) {
+    rubWindow.classList.remove('show');
     setTimeout(() => {rubPopUp.style.opacity = '0';}, 1);
     setTimeout(() => {rubPopUp.style.display = "none";}, 300);
   } else if (event.target == jadPopUp) {
+    jadWindow.classList.remove('show');
     setTimeout(() => {jadPopUp.style.opacity = '0';}, 1);
     setTimeout(() => {jadPopUp.style.display = "none";}, 300);
   } else if (event.target == citPopUp) {
+    citWindow.classList.remove('show');
     setTimeout(() => {citPopUp.style.opacity = '0';}, 1);
     setTimeout(() => {citPopUp.style.display = "none";}, 300);
   } else if (event.target == aquaPopUp) {
+    aquaWindow.classList.remove('show');
     setTimeout(() => {aquaPopUp.style.opacity = '0';}, 1);
     setTimeout(() => {aquaPopUp.style.display = "none";}, 300);
   } else if (event.target == side_Menu) {
